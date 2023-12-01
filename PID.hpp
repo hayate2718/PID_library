d1d6c5114e0bf50aa6ad78613f7a9e0f925888a2
hayate2718 <xxkiaraxx1250@gmail.com>
2023-12-01 18:05:19 +0900

    コメントアウトとれどみを追記した

--- PID.hpp
+++ PID.hpp
@@ -7,9 +7,7 @@
 
 #ifndef INC_PID_HPP_
 #define INC_PID_HPP_
-#include "main.h"
 
-#include <math.h>
 
 class PID
 {
@@ -17,11 +15,14 @@ private:
 	float p; //比例ゲイン
 	float i; //積分ゲイン
 	float d; //微分ゲイン
+	
 	float dt; //微小時間（制御周期）
+	
 	float error; //偏差
 	float i_sum; //積分制御用偏差バッファ
 	float error_before; //前回偏差
-	float p_mv;
+
+	float p_mv; //各PID制御項の操作量
 	float i_mv;
 	float d_mv;
 
@@ -64,6 +65,8 @@ inline PID::PID(float p,float i,float d,float dt,float i_lim,float mv_lim):
 	return;
 }
 
+
+// PIDゲインのセット関数
 inline void PID::PID_set_p(float p){
 	this->p = p;
 }
@@ -76,18 +79,23 @@ inline void PID::PID_set_d(float d){
 	this->d = d;
 }
 
+//PID制御の微小時間項をセット（多くの場合は制御周期）
 inline void PID::PID_set_dt(float dt){
 	this->dt = dt;
 }
 
+//積分制御項のリミッタをセットする関数
 inline void PID::PID_set_i_lim(float lim){
 	this->i_lim = lim;
 }
 
+
+//PID制御全体の操作量のリミッタをセットする関数
 inline void PID::PID_set_mv_lim(float lim){
 	this->mv_lim = lim;
 }
 
+//PIDゲインのゲット関数
 inline float PID::PID_get_p(){
 	float p;
 	p = this->p;
@@ -106,12 +114,13 @@ inline float PID::PID_get_d(){
 	return d;
 }
 
+//PID制御の実体の関数
 inline float PID::PID_controller(float error){
 	float MV = 0; //PIDコントローラ操作量
 
 	i_sum = i_sum + dt*(error+error_before)/2; //微小時間の間線形に動いていたとして
 
-	if(fabsf(i_sum) > this->i_lim){
+	if(fabsf(i_sum) > this->i_lim){ //積分制御のリミッタを適用
 		if(i_sum > 0){
 			i_sum = i_lim;
 		}else{
@@ -119,13 +128,13 @@ inline float PID::PID_controller(float error){
 		}
 	}
 
-	p_mv = this->p*error;
+	p_mv = this->p*error; //各制御ゲインを適用
 	i_mv = this->i*i_sum;
 	d_mv = this->d*(error-error_before)/dt;
 
-	MV = p_mv+i_mv+d_mv;
+	MV = p_mv+i_mv+d_mv; //制御量を決定する
 
-	if(fabsf(MV) > this->mv_lim){
+	if(fabsf(MV) > this->mv_lim){ //制御量のリミッタを適用
 		if(MV > 0){
 			MV = mv_lim;
 		}else{
@@ -138,6 +147,7 @@ inline float PID::PID_controller(float error){
 	return MV;
 }
 
+//積分パラメータのリセット関数
 inline void PID::PID_reset(){
 	i_sum = 0;
 	error_before = 0;
