df1c98244fbb25a1448a4fd5136e05b5c33c9c71
hayate2718 <xxkiaraxx1250@gmail.com>
2025-04-30 20:17:44 +0900

    積分制御アンチワインドアップを強化

--- PID.hpp
+++ PID.hpp
@@ -8,6 +8,7 @@
 #ifndef INC_PID_HPP_
 #define INC_PID_HPP_
 
+#include <math.h>
 
 class PID
 {
@@ -15,6 +16,8 @@ private:
 	float p; //比例ゲイン
 	float i; //積分ゲイン
 	float d; //微分ゲイン
+
+	float c; //アンチワインドアップゲイン
 	
 	float dt; //微小時間（制御周期）
 	
@@ -34,6 +37,8 @@ public:
 	void PID_set_p(float p);
 	void PID_set_i(float i);
 	void PID_set_d(float d);
+	void PID_set_c(float c);
+
 	void PID_set_dt(float dt);
 
 	void PID_set_i_lim(float lim);
@@ -42,6 +47,7 @@ public:
 	float PID_get_p();
 	float PID_get_i();
 	float PID_get_d();
+	float PID_get_c();
 
 	float PID_controller(float error);
 
@@ -52,6 +58,7 @@ inline PID::PID(float p,float i,float d,float dt,float i_lim,float mv_lim):
 		p(p),
 		i(i),
 		d(d),
+		c(1/i),
 		dt(dt),
 		error(0),
 		i_sum(0),
@@ -77,6 +84,10 @@ inline void PID::PID_set_i(float i){
 
 inline void PID::PID_set_d(float d){
 	this->d = d;
+}
+
+inline void PID::PID_set_c(float c) {
+	this->c = c;
 }
 
 //PID制御の微小時間項をセット（多くの場合は制御周期）
@@ -114,9 +125,18 @@ inline float PID::PID_get_d(){
 	return d;
 }
 
+inline float PID::PID_get_c() {
+	float c;
+	c = this->c;
+	return c;
+}
+
 //PID制御の実体の関数
 inline float PID::PID_controller(float error){
 	float MV = 0; //PIDコントローラ操作量
+
+	p_mv = this->p * error; //各制御ゲインを適用
+	d_mv = this->d * (error - error_before) / dt;
 
 	i_sum = i_sum + dt*(error+error_before)/2; //微小時間の間線形に動いていたとして
 
@@ -128,16 +148,16 @@ inline float PID::PID_controller(float error){
 		}
 	}
 
-	p_mv = this->p*error; //各制御ゲインを適用
 	i_mv = this->i*i_sum;
-	d_mv = this->d*(error-error_before)/dt;
 
 	MV = p_mv+i_mv+d_mv; //制御量を決定する
 
 	if(fabsf(MV) > this->mv_lim){ //制御量のリミッタを適用
 		if(MV > 0){
+			i_sum += (MV - mv_lim) * (this->c);
 			MV = mv_lim;
 		}else{
+			i_sum -= (MV + mv_lim) * (this->c);
 			MV = -1*mv_lim;
 		}
 	}
